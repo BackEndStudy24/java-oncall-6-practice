60439723227092449fdad89cd0248e2887ab734e
Teddysir <sdzx0719@naver.com>
2024-12-12 17:59:24 +0900

    feat: 주말 타입 Enum클래스 작성

---
+++ src/main/java/oncall/constants/WeekType.java
@@ -0,0 +1,38 @@
+package oncall.constants;
+
+public enum WeekType {
+
+    MONDAY("월",0),
+    TUESDAY("화",1),
+    WEDNESDAY("수",2),
+    THURSDAY("목",3),
+    FRIDAY("금",4),
+    SATURDAY("토",5),
+    SUNDAY("일",6);
+
+    private final String strDay;
+    private final int day;
+
+    WeekType(String strDay, int day) {
+        this.strDay = strDay;
+        this.day = day;
+    }
+
+    public static String checkedWeek(int day) {
+        WeekType[] weekTypes = WeekType.values();
+        for(WeekType weekType : weekTypes)  {
+            if(weekType.day == (day % 7)) { // 0 -> 월
+                return weekType.strDay;
+            }
+        }
+        return null;
+    }
+
+    public String getStrDay() {
+        return strDay;
+    }
+
+    public int getDay() {
+        return day;
+    }
+}
